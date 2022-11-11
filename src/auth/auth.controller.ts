0912bcfc44c483ba7d5d1b12a4fdf4ab8bb79135
reghay-repo <osm2014@hotmail.fr>
2022-11-11 18:32:32 +0100

    make sign up and sign in features

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,19 @@
-import { Controller, Post, Req } from '@nestjs/common';
-import { Request } from 'express';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto, LoginDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @Post('signin')
-  signin(@Req() req: Request) {
-    console.log(req);
-    return this.authService.signUp();
+  @Post('register')
+  register(@Body() dto: AuthDto) {
+    return this.authService.register(dto);
   }
 
-  @Post('signup')
-  signup() {
-    return this.authService.signUp();
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  login(@Body() dto: LoginDto) {
+    return this.authService.login(dto);
   }
 }
